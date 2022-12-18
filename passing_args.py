739a41d541312f67eaff52da24d1dc53785dcada
RajendraPro <40108967+RajendraPro@users.noreply.github.com>
2022-12-18 22:48:30 +0530

    Create passing_args.py
    add basics methods

---
+++ passing_args.py
@@ -0,0 +1,19 @@
+def HelloFunc(*arg ):
+    print(arg)
+    
+a=1000
+
+param = False
+param_addition = {}
+if param:
+    param_addition  = {"parma_arg_1": "Value"}
+    
+tst = {"a": 10, "b":20}
+HelloFunc({"a": 10}, 
+{"b":20},
+"Hello"  if a == 100 else None,
+{**param_addition}
+)
+
+
+
